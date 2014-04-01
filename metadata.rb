3eec1c4000463d0d51a2e584ba5b120cb7d9548b
Tony Miller <mcfiredrill@gmail.com>
2014-04-01 13:54:13 -0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "liquidsoap-cookbook"
+maintainer        "Tony Miller"
+maintainer_email  "mcfiredrill@gmail.com"
+description       "Installs liquidsoap and all ocaml dependencies"
+version           "0.0.1"
+
+recipe "liquidosap", "Installs liquidsoap"
+
+supports "ubuntu"
